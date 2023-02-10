571c04baddc1879f476cf9673767a589012741cf
Frandey <frandeybriceno@contelonline.com>
2023-02-10 09:34:08 -0400

    Commit Inicial

---
+++ src/prepaso2/PrepaSO2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prepaso2;
+
+/**
+ *
+ * @author Franda
+ */
+public class PrepaSO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           System.out.println("Inicializando y conectando con GitHub");
+        
+    }
+    
+}
